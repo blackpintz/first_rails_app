7ba62fc4f64978d10d51bbafccc944f8f2816316
Ubuntu <ubuntu@ip-172-31-26-232.us-east-2.compute.internal>
2020-06-04 10:58:53 +0000

    configure secure password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_081710) do
+ActiveRecord::Schema.define(version: 2020_06_04_103624) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_06_04_081710) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
